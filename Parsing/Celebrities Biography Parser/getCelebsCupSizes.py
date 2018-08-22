fcc8fad24824d8dcc2e7fc0d918916b99f1ce335
Ahmed Meguid <ameguid12@gmail.com>
2018-08-22 04:19:27 +0200

    new folder

---
+++ Parsing/Celebrities Biography Parser/getCelebsCupSizes.py
@@ -0,0 +1,13 @@
+from lxml import html
+import requests
+
+def getCelebsCupSizes() :
+    pageLink = "http://www.famousfix.com/list/celebrities-by-bra-cup-size"
+    searchLink = "http://www.famousfix.com/list/celebrities-with-bra-cup-size-"
+    page = requests.get(pageLink)
+    splittedUrls = page.content.split(searchLink)
+    celebsCupSizes = set()
+    for i in range(1,len(splittedUrls)) :
+        cupSize = splittedUrls[i].split("\"",1)[0]
+        celebsCupSizes.add(cupSize)
+    return list(celebsCupSizes)
---
+++ Parsing/Celebrities Biography Parser/getCelebsNames.py
@@ -0,0 +1,23 @@
+from lxml import html
+import requests
+
+from getCelebsCupSizes import getCelebsCupSizes
+
+def getCelebsNames(cupSizes) :
+    fullCelebsList = set()
+    fullCelebsFile = open("celebs.txt", "w")
+    fullCelebsFileNumbered = open("celebs_numbered.txt", "w")
+    for cupsize in cupSizes:
+        celebsFile = open("Celebrities Names By Cup Size/celebs_" + cupsize + ".txt", "r")
+        celebs = celebsFile.readlines()
+        for celeb in celebs:
+            if celeb not in fullCelebsList:
+                fullCelebsList.add(celeb)
+    index = 1
+    for celeb in sorted(fullCelebsList):
+        fullCelebsFile.write(celeb)
+        fullCelebsFileNumbered.write(str(index) + ": " + celeb)
+        index += 1
+
+getCelebsNames(getCelebsCupSizes())
+
